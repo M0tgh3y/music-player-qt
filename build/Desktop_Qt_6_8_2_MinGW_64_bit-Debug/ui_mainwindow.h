fcb9a2b1316db87e75b5035b6dcc928296930c55
M0tgh3y <mot13832004@gmail.com>
2025-04-06 09:21:23 +0330

    add icon and text

--- build/Desktop_Qt_6_8_2_MinGW_64_bit-Debug/ui_mainwindow.h
+++ build/Desktop_Qt_6_8_2_MinGW_64_bit-Debug/ui_mainwindow.h
@@ -12,6 +12,7 @@
 #include <QtCore/QVariant>
 #include <QtGui/QIcon>
 #include <QtWidgets/QApplication>
+#include <QtWidgets/QFrame>
 #include <QtWidgets/QLabel>
 #include <QtWidgets/QMainWindow>
 #include <QtWidgets/QWidget>
@@ -23,6 +24,9 @@ class Ui_musicplayer
 public:
     QWidget *centralwidget;
     QLabel *backgroundimage;
+    QFrame *titleframe;
+    QLabel *label;
+    QLabel *label_2;
 
     void setupUi(QMainWindow *musicplayer)
     {
@@ -37,7 +41,27 @@ public:
         backgroundimage = new QLabel(centralwidget);
         backgroundimage->setObjectName("backgroundimage");
         backgroundimage->setGeometry(QRect(0, 0, 880, 550));
+        QFont font;
+        font.setBold(true);
+        backgroundimage->setFont(font);
         backgroundimage->setStyleSheet(QString::fromUtf8("background-color: rgb(17, 47, 76);"));
+        titleframe = new QFrame(centralwidget);
+        titleframe->setObjectName("titleframe");
+        titleframe->setGeometry(QRect(0, 0, 880, 40));
+        titleframe->setFrameShape(QFrame::Shape::StyledPanel);
+        titleframe->setFrameShadow(QFrame::Shadow::Raised);
+        label = new QLabel(titleframe);
+        label->setObjectName("label");
+        label->setGeometry(QRect(5, 0, 40, 40));
+        label->setPixmap(QPixmap(QString::fromUtf8(":/image/icons/music.png")));
+        label->setScaledContents(true);
+        label_2 = new QLabel(titleframe);
+        label_2->setObjectName("label_2");
+        label_2->setGeometry(QRect(50, 0, 151, 40));
+        QFont font1;
+        font1.setPointSize(16);
+        font1.setBold(true);
+        label_2->setFont(font1);
         musicplayer->setCentralWidget(centralwidget);
 
         retranslateUi(musicplayer);
@@ -48,7 +72,9 @@ public:
     void retranslateUi(QMainWindow *musicplayer)
     {
         musicplayer->setWindowTitle(QCoreApplication::translate("musicplayer", "Music Player", nullptr));
-        backgroundimage->setText(QCoreApplication::translate("musicplayer", "TextLabel", nullptr));
+        backgroundimage->setText(QString());
+        label->setText(QString());
+        label_2->setText(QCoreApplication::translate("musicplayer", "Mot Player", nullptr));
     } // retranslateUi
 
 };
