119396456bc3e2aac14c56f03389edf16cf96b24
M0tgh3y <mot13832004@gmail.com>
2025-04-07 10:59:27 +0330

    pink

--- build/Desktop_Qt_6_8_2_MinGW_64_bit-Debug/ui_mainwindow.h
+++ build/Desktop_Qt_6_8_2_MinGW_64_bit-Debug/ui_mainwindow.h
@@ -15,7 +15,6 @@
 #include <QtWidgets/QFrame>
 #include <QtWidgets/QLabel>
 #include <QtWidgets/QMainWindow>
-#include <QtWidgets/QPushButton>
 #include <QtWidgets/QWidget>
 
 QT_BEGIN_NAMESPACE
@@ -25,20 +24,17 @@ class Ui_musicplayer
 public:
     QWidget *centralwidget;
     QLabel *backgroundimage;
-    QFrame *titleframe;
-    QLabel *icon;
-    QLabel *motplayer;
-    QPushButton *close;
-    QPushButton *mini;
+    QFrame *frame;
+    QLabel *label;
 
     void setupUi(QMainWindow *musicplayer)
     {
         if (musicplayer->objectName().isEmpty())
             musicplayer->setObjectName("musicplayer");
         musicplayer->resize(880, 550);
-        QIcon icon1;
-        icon1.addFile(QString::fromUtf8(":/image/icons/music2.png"), QSize(), QIcon::Mode::Normal, QIcon::State::Off);
-        musicplayer->setWindowIcon(icon1);
+        QIcon icon;
+        icon.addFile(QString::fromUtf8(":/image/icons/music2.png"), QSize(), QIcon::Mode::Normal, QIcon::State::Off);
+        musicplayer->setWindowIcon(icon);
         centralwidget = new QWidget(musicplayer);
         centralwidget->setObjectName("centralwidget");
         backgroundimage = new QLabel(centralwidget);
@@ -47,40 +43,20 @@ public:
         QFont font;
         font.setBold(true);
         backgroundimage->setFont(font);
-        backgroundimage->setStyleSheet(QString::fromUtf8("background-color: rgb(17, 47, 76);"));
-        titleframe = new QFrame(centralwidget);
-        titleframe->setObjectName("titleframe");
-        titleframe->setGeometry(QRect(0, 0, 880, 40));
-        titleframe->setFrameShape(QFrame::Shape::StyledPanel);
-        titleframe->setFrameShadow(QFrame::Shadow::Raised);
-        icon = new QLabel(titleframe);
-        icon->setObjectName("icon");
-        icon->setGeometry(QRect(5, 0, 40, 40));
-        icon->setPixmap(QPixmap(QString::fromUtf8(":/image/icons/music.png")));
-        icon->setScaledContents(true);
-        motplayer = new QLabel(titleframe);
-        motplayer->setObjectName("motplayer");
-        motplayer->setGeometry(QRect(50, 0, 151, 40));
-        QFont font1;
-        font1.setFamilies({QString::fromUtf8("Arial")});
-        font1.setPointSize(16);
-        font1.setBold(false);
-        font1.setItalic(false);
-        motplayer->setFont(font1);
-        motplayer->setStyleSheet(QString::fromUtf8("font: 16pt \"Arial\";\n"
-"color: rgb(85, 170, 255);"));
-        close = new QPushButton(titleframe);
-        close->setObjectName("close");
-        close->setGeometry(QRect(830, 5, 40, 30));
-        QIcon icon2;
-        icon2.addFile(QString::fromUtf8(":/image/icons/Delete-16.png"), QSize(), QIcon::Mode::Normal, QIcon::State::Off);
-        close->setIcon(icon2);
-        mini = new QPushButton(titleframe);
-        mini->setObjectName("mini");
-        mini->setGeometry(QRect(785, 5, 40, 30));
-        QIcon icon3;
-        icon3.addFile(QString::fromUtf8(":/image/icons/Minus-16.png"), QSize(), QIcon::Mode::Normal, QIcon::State::Off);
-        mini->setIcon(icon3);
+        backgroundimage->setStyleSheet(QString::fromUtf8("background-color: rgb(255, 170, 255);\n"
+""));
+        frame = new QFrame(centralwidget);
+        frame->setObjectName("frame");
+        frame->setGeometry(QRect(20, 20, 300, 250));
+        frame->setStyleSheet(QString::fromUtf8("background-color: rgb(255, 85, 255);\n"
+"border-radius: 10px;"));
+        frame->setFrameShape(QFrame::Shape::StyledPanel);
+        frame->setFrameShadow(QFrame::Shadow::Raised);
+        label = new QLabel(frame);
+        label->setObjectName("label");
+        label->setGeometry(QRect(10, 10, 30, 30));
+        label->setStyleSheet(QString::fromUtf8("background-color: rgba(0, 0, 0, 0);"));
+        label->setPixmap(QPixmap(QString::fromUtf8(":/image/icons/Audio Wave-32.png")));
         musicplayer->setCentralWidget(centralwidget);
 
         retranslateUi(musicplayer);
@@ -92,10 +68,7 @@ public:
     {
         musicplayer->setWindowTitle(QCoreApplication::translate("musicplayer", "Music Player", nullptr));
         backgroundimage->setText(QString());
-        icon->setText(QString());
-        motplayer->setText(QCoreApplication::translate("musicplayer", "Mot Player", nullptr));
-        close->setText(QString());
-        mini->setText(QString());
+        label->setText(QString());
     } // retranslateUi
 
 };
